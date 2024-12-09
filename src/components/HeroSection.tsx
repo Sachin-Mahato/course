19094c8082c7de0275b97ce21235790e898d1bf3
sachin <sachin333777@outlook.com>
2024-12-09 02:03:03 +0000

    change routes link

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,7 +21,7 @@ function HeroSection() {
         </p>
 
         <div className="mt-4">
-          <Link href={"/course"}>
+          <Link href={"/courses"}>
             <Button
               borderRadius="1.75rem"
               className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,23 +12,29 @@ function Navbar({ className }: { className?: string }) {
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-          <MenuItem setActive={setActive} active={active} item="Home">
-          </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
         </Link>
         <MenuItem setActive={setActive} active={active} item="All Courses">
           <div className="flex flex-col justify-center items-center gap-2 transparen">
             <HoveredLink href="/courses">All Courses</HoveredLink>
-            <HoveredLink href="/courses/kotlin">Kotlin Development</HoveredLink>
-            <HoveredLink href="/courses/rust">Rust Programming</HoveredLink>
-            <HoveredLink href="/courses/go">Go Programming</HoveredLink>
-            <HoveredLink href="/courses/scala">Scala Programming</HoveredLink>
-            <HoveredLink href="/courses/elixir">Elixir Development</HoveredLink>
-            <HoveredLink href="/courses/lua">Lua Scripting</HoveredLink>
+            <HoveredLink href="/courses">Kotlin Development</HoveredLink>
+            <HoveredLink href="/courses">Rust Programming</HoveredLink>
+            <HoveredLink href="/courses">Go Programming</HoveredLink>
+            <HoveredLink href="/courses">Scala Programming</HoveredLink>
+            <HoveredLink href="/courses">Elixir Development</HoveredLink>
+            <HoveredLink href="/courses">Lua Scripting</HoveredLink>
           </div>
         </MenuItem>
         <Link href={"/contact"}>
-          <MenuItem setActive={setActive} active={active} item="Contact Us">
-          </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
         </Link>
       </Menu>
     </nav>
